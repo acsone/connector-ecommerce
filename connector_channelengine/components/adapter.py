0a77872500aa32eb9bf1ad426fbad85ee4fab59b
nans <nans.lefebvre@acsone.eu>
2020-09-21 13:43:58 +0200

    [IMP] connector_channelengine: export parents

--- connector_channelengine/components/adapter.py
+++ connector_channelengine/components/adapter.py
@@ -14,6 +14,13 @@ _logger = logging.getLogger(__name__)
 
 
 class ChannelEngineAdapter(Component):
+    """We can export at most 10000 products at a time.
+       However, we need to enrich the data with parent's data (essentially the template)
+       if the products have sibling variants. In the worst case, for products we export
+       we have one parent, but we are updating only one variant for each; so to be sure
+       we need to stop at 5000.
+    """
+
     _name = "channelengine.adapter"
     _inherit = ["base.backend.adapter", "channelengine.connector"]
     _usage = "backend.adapter"
@@ -31,15 +38,30 @@ class ChannelEngineAdapter(Component):
         """
         return ProductApi(self._get_api_client())
 
+    def _export_parents(self, bindings, product_data):
+        parents = []
+        processed = set()
+        for b, pd in zip(bindings, product_data):
+            parent_no = pd.get("ParentMerchantProductNo")
+            if parent_no and parent_no not in processed:
+                processed.add(parent_no)
+                parent_json = {}
+                parent_json["Name"] = b.product_id.product_tmpl_id.display_name
+                parent_json["MerchantProductNo"] = parent_no
+                parents.append(parent_json)
+        return parents
+
     def export(self, bindings):
         done, warning, exception = (
             bindings.browse(),
             bindings.browse(),
             bindings.browse(),
         )
-        if len(bindings):  # TODO!!:batch by 10000
+        if len(bindings):  # TODO!!:batch by 5000
             client = self._get_product_client()
-            data = bindings.mapped("data")
+            product_data = bindings.mapped("data")
+            parent_data = self._export_parents(bindings, product_data)
+            data = parent_data + product_data
             try:
                 response = client.product_create(merchant_product_request=data)
             except ApiException as e:
--- connector_channelengine/models/product.py
+++ connector_channelengine/models/product.py
@@ -1,6 +1,8 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import os.path as path
+
 from odoo import api, fields, models
 
 
@@ -27,6 +29,14 @@ class Product(models.Model):
         help="Whether this product needs to be checked for backend domains.",
     )
     category_trail = fields.Char(compute="_compute_category_trail", store=True)
+    channelengine_parent = fields.Char(
+        compute="_compute_merchant_parent",
+        store=True,
+        readonly=True,
+        default=False,
+        help="Technical field used by ChannelEngine."
+        "This value is set iff the product has more than one variant.",
+    )
 
     def write(self, vals):
         """Whenever we write on a product, it might move the product in or out
@@ -41,3 +51,16 @@ class Product(models.Model):
         for record in self:
             category_name = record.categ_id.complete_name or "_"
             record.category_trail = category_name.replace("/", ">")
+
+    @api.depends("product_tmpl_id.product_variant_ids.default_code")
+    def _compute_merchant_parent(self):
+        for rec in self:
+            if len(rec.product_tmpl_id.product_variant_ids) > 1:
+                parent = "P{}".format(rec.product_tmpl_id.id)
+                codes = rec.product_tmpl_id.product_variant_ids.mapped("default_code")
+                nonempty_codes = [c for c in codes if c]
+                prefix = path.commonprefix(nonempty_codes)
+                parent = "{}_{}".format(parent, prefix) if prefix else parent
+                rec.channelengine_parent = parent
+            else:
+                rec.channelengine_parent = False
